15150f181905433b4364a17e7a08a77ec313011a
PrathmPatil <prathmpatil2818@gmail.com>
2022-11-09 15:06:11 +0530

    selection practice problems

---
+++ programs/add.sh
@@ -0,0 +1,13 @@
+# Prooblem -> How to Generate Random Number using shell script?
+#!/bin/bash
+#num1=$((RANDOM%5));
+#num2=$((RANDOM%5+1));
+
+#echo $num1;
+#echo $num2;
+
+#num3=$((RANDOM%90+10));
+#echo $num3;
+
+num4=$((RANDOM%900+100))
+echo $num4;
---
+++ programs/user.sh
@@ -0,0 +1,5 @@
+read -p "Enter first number:" x;
+read -p "Enter second number:" y;
+
+Number=$(($x+$y));
+echo $Number;
